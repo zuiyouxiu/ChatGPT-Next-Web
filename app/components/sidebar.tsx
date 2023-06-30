1a65d5ae9b98f2ae47b2ec30e89cd90595a98c47
zuiyouxiu <55024485+zuiyouxiu@users.noreply.github.com>
2023-06-30 16:46:45 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT For XVX
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          GPT4资源宝贵，建议按需开启
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
